ae5f5b01d92c375e14d250acf92379665e093cdb
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-04-30 18:09:03 -0500

    error class

---
+++ src/error.py
@@ -0,0 +1,13 @@
+class Error:
+    def __init__(self, _pos_start, _pos_end, _name, _details):
+        self.name = _name
+        self.pos_start = _pos_start
+        self.pos_end = _pos_end
+        self.details = _details
+
+    def __repr__(self):
+        return '  {}: {}\n  File{}, line{}'.format(self.name, self.details, self.pos_start.fn, self.pos_start.ln + 1)
+
+class IllegalCharError(Error):
+    def __init__(self, _pos_start, _pos_end, details):
+        super().__init__(_pos_start, _pos_end, 'Illegal Character', details)
