c17330c89eb8501f0dd21ee4e5309674e4d53e4e
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-05-08 17:10:12 -0500

    NumberNode Class

--- src/node.py
+++ src/node.py
@@ -1,6 +1,9 @@
 class NumberNode:
 	def __init__(self, _token):
 		self.token = _token
+
+		self.pos_start = self.token.pos_start
+		self.pos_end = self.token.pos_end
 	
 	def __repr__(self):
 		return '{}'.format(self.token)
@@ -11,6 +14,9 @@ class BinOpNode:
 		self.op_token = _op_token
 		self.right_node = _right_node
 
+		self.pos_start = self.left_node.pos_start
+		self.pos_end = self.right_node.pos_end
+
 	def __repr__(self):
 		return '({},{},{})'.format(self.left_node, self.op_token, self.right_node)
 
@@ -18,6 +24,9 @@ class UnaryOpNode:
 	def __init__(self, _op_token, _node):
 		self.op_token = _op_token
 		self.node = _node
+
+		self.pos_start = self.op_token.pos_start
+		self.pos_end = _node.pos_end
 
 	def __repr__(self):
 		return '({},{})'.format(self.op_token, self.node)
