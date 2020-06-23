6c59f6bdc3c8156bf745c4945e53780bbc02a788
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-06-23 15:27:27 -0500

    nodes in parser tree

--- src/nodes.py
+++ src/nodes.py
@@ -1,6 +1,6 @@
 class NumberNode:
-	def __init__(self, _token):
-		self.token = _token
+	def __init__(self, token):
+		self.token = token
 
 		self.pos_start = self.token.pos_start
 		self.pos_end = self.token.pos_end
@@ -9,25 +9,25 @@ class NumberNode:
 		return '{}'.format(self.token)
 
 class VarAccessNode:
-	def __init__(self, _var_name_token):
-		self.var_name_token = _var_name_token
+	def __init__(self, var_name_token):
+		self.var_name_token = var_name_token
 
 		self.pos_start = self.var_name_token.pos_start
 		self.pos_end = self.var_name_token.pos_end
 
 class VarAssignNode:
-	def __init__(self, _var_name_token, _value_node):
-		self.var_name_token = _var_name_token
+	def __init__(self, var_name_token, _value_node):
+		self.var_name_token = var_name_token
 		self.value_node = _value_node
 
 		self.pos_start = self.var_name_token.pos_start
 		self.pos_end = self.value_node.pos_end
 
 class BinOpNode:
-	def __init__(self, _left_node, _op_token, _right_node):
-		self.left_node = _left_node
-		self.op_token = _op_token
-		self.right_node = _right_node
+	def __init__(self, left_node, op_token, right_node):
+		self.left_node = left_node
+		self.op_token = op_token
+		self.right_node = right_node
 
 		self.pos_start = self.left_node.pos_start
 		self.pos_end = self.right_node.pos_end
@@ -36,48 +36,48 @@ class BinOpNode:
 		return '({},{},{})'.format(self.left_node, self.op_token, self.right_node)
 
 class UnaryOpNode:
-	def __init__(self, _op_token, _node):
-		self.op_token = _op_token
-		self.node = _node
+	def __init__(self, op_token, node):
+		self.op_token = op_token
+		self.node = node
 
 		self.pos_start = self.op_token.pos_start
-		self.pos_end = _node.pos_end
+		self.pos_end = node.pos_end
 
 	def __repr__(self):
 		return '({},{})'.format(self.op_token, self.node)
 
 class IfNode:
-	def __init__(self, _cases, _else_case):
-		self.cases = _cases
-		self.else_case = _else_case
+	def __init__(self, cases, else_case):
+		self.cases = cases
+		self.else_case = else_case
 
 		self.pos_start = self.cases[0][0].pos_start
 		self.pos_end = (self.else_case or self.cases[len(self.cases) - 1][0]).pos_end
 
 class ForNode:
-	def __init__(self, _var_name_token, _start_value_node, _end_value_node, _step_value_node, _body_node):
-		self.var_name_token = _var_name_token
-		self.start_value_node = _start_value_node
-		self.end_value_node = _end_value_node
+	def __init__(self, var_name_token, start_value_node, end_value_node, _step_value_node, body_node):
+		self.var_name_token = var_name_token
+		self.start_value_node = start_value_node
+		self.end_value_node = end_value_node
 		self.step_value_node = _step_value_node
-		self.body_node = _body_node
+		self.body_node = body_node
 
 		self.pos_start = self.var_name_token.pos_start
 		self.pos_end = self.body_node.pos_end
 
 class WhileNode:
-	def __init__(self, _condition_node, _body_node):
-		self.condition_node = _condition_node
-		self.body_node = _body_node
+	def __init__(self, condition_node, body_node):
+		self.condition_node = condition_node
+		self.body_node = body_node
 
 		self.pos_start = self.condition_node.pos_start
 		self.pos_end = self.body_node.pos_end
 
 class FunctionNode:
-	def __init__(self, _var_name_token, _arg_name_tokens, _body_node):
-		self.var_name_token = _var_name_token
-		self.arg_name_tokens = _arg_name_tokens
-		self.body_node = _body_node
+	def __init__(self, var_name_token, arg_name_tokens, body_node):
+		self.var_name_token = var_name_token
+		self.arg_name_tokens = arg_name_tokens
+		self.body_node = body_node
 
 		if(self.var_name_token):
 			self.pos_start = self.var_name_token.pos_start
@@ -89,9 +89,9 @@ class FunctionNode:
 		self.pos_end = self.body_node.pos_end
 
 class CallNode:
-	def __init__(self, _node_to_call, _arg_nodes):
-		self.node_to_call = _node_to_call
-		self.arg_nodes = _arg_nodes
+	def __init__(self, node_to_call, arg_nodes):
+		self.node_to_call = node_to_call
+		self.arg_nodes = arg_nodes
 
 		self.pos_start = self.node_to_call.pos_start
 
--- src/values.py
+++ src/values.py
@@ -1,6 +1,5 @@
 from .errors import RunTimeError
 from .results import RunTimeResult
-from .interpreter import Interpreter, Context
 from .symbol_table import SymbolTable
 
 class Value:
@@ -181,53 +180,3 @@ class Number(Value):
 	
 	def __repr__(self):
 		return str(self.value)
-
-class Function(Value):
-	def __init__(self, name, body_node, arg_names):
-		super().__init__()
-		self.name = name or "<anonymous>"
-		self.body_node = body_node
-		self.arg_names = arg_names
-
-	def execute(self, args):
-		res = RunTimeResult()
-
-		interpreter = Interpreter()
-		new_context = Context(self.name, self.context, self.pos_start)
-		new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
-
-		if(len(args) > len(self.arg_names)):
-			return res.failure(RunTimeError(
-				self.pos_start, self.pos_end,
-				"'{}' too many args passed into '{}'".format(len(args) - len(self.arg_names), self.name),
-				self.context
-			))
-		
-		if(len(args) < len(self.arg_names)):
-			return res.failure(RunTimeError(
-				self.pos_start, self.pos_end,
-				"{} too few args passed into '{}'".format(len(self.arg_names) - len(args), self.name),
-				self.context
-			))
-
-		for i in range(len(args)):
-			arg_name = self.arg_names[i]
-			arg_value = args[i]
-			arg_value.set_context(new_context)
-			new_context.symbol_table.set(arg_name, arg_value)
-
-		value = res.register(interpreter.visit(self.body_node, new_context))
-
-		if(res.error):
-			return res
-
-		return res.success(value)
-
-	def copy(self):
-		copy = Function(self.name, self.body_node, self.arg_names)
-		copy.set_context(self.context)
-		copy.set_pos(self.pos_start, self.pos_end)
-		return copy
-
-	def __repr__(self):
-		return '<function {}>'.format(self.name)
