4444f48de529e2aba8240d09d2e849a7df0315d7
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-07-10 23:24:20 -0500

    String node

--- src/nodes.py
+++ src/nodes.py
@@ -8,6 +8,16 @@ class NumberNode:
 	def __repr__(self):
 		return '{}'.format(self.token)
 
+class StringNode:
+	def __init__(self, token):
+		self.token = token
+
+		self.pos_start = self.token.pos_start
+		self.pos_end = self.token.pos_end
+	
+	def __repr__(self):
+		return '{}'.format(self.token)
+
 class VarAccessNode:
 	def __init__(self, var_name_token):
 		self.var_name_token = var_name_token
