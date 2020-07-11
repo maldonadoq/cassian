443f00b56e4cac48420855dc7e96bab690092714
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-07-10 23:25:04 -0500

    values node

--- src/values.py
+++ src/values.py
@@ -16,7 +16,7 @@ class Value:
 		self.context = context
 		return self
 
-	def added_to(self, other):
+	def added_by(self, other):
 		return None, self.illegal_operation(other)
 
 	def subbed_by(self, other):
@@ -180,3 +180,34 @@ class Number(Value):
 	
 	def __repr__(self):
 		return str(self.value)
+
+
+class String(Value):
+	def __init__(self, value):
+		super().__init__()
+		self.value = value
+
+	def added_by(sel, other):
+		if(isinstance(other, String)):
+			return String(self.value + other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
+
+	def multed_by(self, other):
+		if(isinstance(other, Number)):
+			return String(self.value * other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
+
+	def is_true(self):
+		return len(self.value) > 0
+
+	def copy(self):
+		copy = String(self.value)
+		copy.set_pos(self.pos_start, self.pos_end)
+		copy.set_context(self.context)
+
+		return copy
+
+	def __repr__(self):
+		return self.value
